a8f9fb5c7c9fc65717a8c47b63793127d5ccf377
Abhinav <abhinavroy2099@gmail.com>
2024-11-15 13:39:22 +0530

    imae

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,15 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
-    domains: ["www.ramanship.com"],
+    domains: [
+      "res.cloudinary.com",
+      "d1be4virimwdjl.cloudfront.net",
+      "images.unsplash.com",
+      "cdn.openart.ai",
+      "www.pixelstalk.net",
+      "static.toiimg.com",
+      "img.budgettravel.com",
+    ],
   },
 };
 
